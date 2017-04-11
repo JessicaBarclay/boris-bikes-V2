6eea313021c5deb82fb6c5998811a2fda8ec4355
SimonTanner <simonthetanner@hotmail.com>
2017-04-11 14:31:57 +0000

    Test & docking_station updated

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,10 +6,15 @@ describe DockingStation do
     it 'releases working bikes' do
         bike = subject.release_bike
         expect(bike).to be_working
-
-
+    end
+    
+    it 'returns docked bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect (subject.bike).to eq bike
     end
 
-    it { is_expected.to respond_to (:dock).with(1).argument }
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    
     it { is_expected.to respond_to(:bike) }
 end
