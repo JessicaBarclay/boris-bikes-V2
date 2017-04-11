db167164a7bdde574a913384b373692542481189
SimonTanner <simonthetanner@hotmail.com>
2017-04-11 11:16:56 +0000

    Spec & DockingStation.rb updated

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,3 +1,8 @@
+require 'DockingStation'
+
 describe 'DockingStation' do
+    it 'DockingStation responds_to release_bike' do
+        expect(docking_station.release_bike()).to eq nil
+    end
     
 end
