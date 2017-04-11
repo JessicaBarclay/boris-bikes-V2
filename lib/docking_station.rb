7519c64d0419b85d99832a23120a35f2109b6a9f
SimonTanner <simonthetanner@hotmail.com>
2017-04-11 14:37:41 +0000

    Ex11 Bike attr_reader added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,19 @@
 require './lib/Bike'
 
 class DockingStation
+    
+    attr_reader :bike
 
     def release_bike
         Bike.new
     end
 
     def dock(bike)
+        
+        @bike = bike
 
     end
 
-    def bike
-
-    end
 end
 
 #docking_station = DockingStation.new
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -11,7 +11,7 @@ describe DockingStation do
     it 'returns docked bikes' do
         bike = Bike.new
         subject.dock(bike)
-        expect (subject.bike).to eq bike
+        expect(subject.bike).to eq bike
     end
 
     it { is_expected.to respond_to(:dock).with(1).argument }
