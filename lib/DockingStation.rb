1af819fdb58da7df2050a28be6da6419a02d9307
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-11 15:13:30 +0100

    Add bike method

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,14 +1,18 @@
 require './lib/Bike'
 
 class DockingStation
-    
+
     def release_bike
         Bike.new
     end
 
     def dock(bike)
-        
+
+    end
+
+    def bike
+
     end
 end
 
-docking_station = DockingStation.new
+#docking_station = DockingStation.new
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 require './lib/Bike'
 
 class DockingStation
-    
+
     def release_bike
         Bike.new
     end
 
     def dock(bike)
-        
+
+    end
+
+    def bike
+
     end
 end
 
